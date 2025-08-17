32f61f731f23cd31664e4ebd2ccacbb07dde315d
michaelkyalo <michaelkyalo876@gmail.com>
2025-08-17 14:04:29 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
+
+
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  
+        return
+
+    if length == 1:
+        print([0])  
+        return
+
+
+    fib_sequence = [0, 1]
+
+ 
+    while len(fib_sequence) < length:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+
+    print(fib_sequence)
